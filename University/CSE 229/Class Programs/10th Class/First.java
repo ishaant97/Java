c738f8f466e68dba636d78e6d3949cd8a106f77d
Ishaant Kumar Singh <ishaant97@gmail.com>
2023-09-01 11:52:40 +0530

    10th Class Done

---
+++ University/CSE 229/Class Programs/10th Class/First.java
@@ -0,0 +1,24 @@
+class Student {
+    int age;
+    int standard;
+    int section;
+}
+
+class First {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        s1.age = 25;
+        s1.standard = 6;
+        s1.section = '2';
+
+        s2.age = 2;
+        s2.standard = 7;
+        s2.section = '5';
+
+        int[] arr = new int[2];
+        
+
+    }
+}
