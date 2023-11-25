623de240f3fb75bb77460aa22334f6b83ab3a074
Ishaant Kumar Singh <ishaant97@gmail.com>
2023-11-26 01:54:33 +0530

    Stack String Reversal Done

---
+++ University/DSA ETP Practice/Stack/StringReversal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class StringReversal {
+    public static void main(String[] args) {
+        Stack<Character> st = new Stack<>();
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the string : ");
+        String s = in.nextLine();
+        StringBuilder sReversed = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            st.push(s.charAt(i));
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            sReversed.append(st.pop());
+        }
+
+        System.out.println("The reversed string is : " + sReversed);
+
+        in.close();
+    }
+}
